84db219239c01201fdb90bbecff41fd9d7a87901
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-11-04 15:41:49 +0530

    Home UI

--- app/src/main/java/com/pranavprksn/hedp/SplashScreen.kt
+++ app/src/main/java/com/pranavprksn/hedp/SplashScreen.kt
@@ -1,7 +1,11 @@
 package com.pranavprksn.hedp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.airbnb.lottie.LottieAnimationView
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,6 +13,21 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
 
 
+        val tapView = findViewById<TextView>(R.id.tap)
+        val animView = findViewById<LottieAnimationView>(R.id.animationView)
 
+        tapView.setOnClickListener{
+            Toast.makeText(applicationContext, "Setting up!", Toast.LENGTH_SHORT).show()
+
+            intent = Intent(applicationContext, Home::class.java)
+            startActivity(intent)
+        }
+
+        animView.setOnClickListener{
+            Toast.makeText(applicationContext, "Setting up!", Toast.LENGTH_SHORT).show()
+
+            intent = Intent(applicationContext, Home::class.java)
+            startActivity(intent)
+        }
     }
 }
