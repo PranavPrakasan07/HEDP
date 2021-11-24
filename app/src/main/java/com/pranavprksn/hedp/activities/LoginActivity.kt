5d3ed3d8a8b6df63f14e3ec6722caba881092141
Pranav Prakasan <pranav.prakasan2019@gmail.com>
2021-11-24 14:16:03 +0530

    login to kt

--- app/src/main/java/com/pranavprksn/hedp/activities/LoginActivity.kt
+++ app/src/main/java/com/pranavprksn/hedp/activities/LoginActivity.kt
@@ -1,158 +1,138 @@
-package com.pranavprksn.hedp.activities;
+package com.pranavprksn.hedp.activities
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.pranavprksn.hedp.ChatHome
+import com.pranavprksn.hedp.R
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
-
-import com.google.android.gms.auth.api.signin.GoogleSignIn;
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-import com.google.android.gms.auth.api.signin.GoogleSignInClient;
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthCredential;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.GoogleAuthProvider;
-import com.pranavprksn.hedp.ChatHome;
-import com.pranavprksn.hedp.R;
-
-public class LoginActivity extends AppCompatActivity {
-
-    AppCompatButton loginButton;
-    TextView signupLink;
-
-    EditText email, password;
-
-    private static final int RC_SIGN_IN = 1;
-
-    GoogleSignInClient mGoogleSignInClient;
-    static FirebaseAuth auth;
-
-    public static String userid = null;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = auth.getCurrentUser();
-
+class LoginActivity : AppCompatActivity() {
+    var loginButton: AppCompatButton? = null
+    var signupLink: TextView? = null
+    var email: EditText? = null
+    var password: EditText? = null
+    var mGoogleSignInClient: GoogleSignInClient? = null
+    public override fun onStart() {
+        super.onStart()
+        val currentUser = auth!!.currentUser
         if (currentUser != null) {
-            startActivity(new Intent(getApplicationContext(), ChatHome.class));
+            startActivity(Intent(applicationContext, ChatHome::class.java))
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
                 // Google Sign In was successful, authenticate with Firebase
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                assert account != null;
-                Log.d("TAG", "firebaseAuthWithGoogle:" + account.getId());
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("TAG", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken)
+            } catch (e: ApiException) {
                 // Google Sign In failed, update UI appropriately
-                Log.w("TAG", "Google sign in failed", e);
+                Log.w("TAG", "Google sign in failed", e)
             }
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-
-        signupLink = findViewById(R.id.signup_link);
-
-        loginButton = findViewById(R.id.login_button);
-        auth = FirebaseAuth.getInstance();
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        email = findViewById(R.id.email)
+        password = findViewById(R.id.password)
+        signupLink = findViewById(R.id.signup_link)
+        loginButton = findViewById(R.id.login_button)
+        auth = FirebaseAuth.getInstance()
 
         // Configure Google Sign In
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken("324570600050-mmlibt48s2ij5ga5e8cm8m60nmktoubb.apps.googleusercontent.com")
-                .requestEmail()
-                .build();
-
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
-
-        findViewById(R.id.google_button).setOnClickListener(view -> {
-            if (view.getId() == R.id.google_button) {
-                signIn();
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken("324570600050-mmlibt48s2ij5ga5e8cm8m60nmktoubb.apps.googleusercontent.com")
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        findViewById<View>(R.id.google_button).setOnClickListener { view: View ->
+            if (view.id == R.id.google_button) {
+                signIn()
             }
-        });
+        }
 
-        loginButton.setOnClickListener(v -> {
-            String email_text = email.getText().toString();
-            String password_text = password.getText().toString();
+        loginButton?.setOnClickListener(View.OnClickListener { v: View? ->
+            val email_text = email?.text.toString()
+            val password_text = password?.text.toString()
+            if (email_text == "") {
+                Toast.makeText(this, "Enter your email", Toast.LENGTH_SHORT).show()
+            }
+            if (password_text == "") {
+                Toast.makeText(this, "Enter your password", Toast.LENGTH_SHORT).show()
+            }
+            if (!(email_text == "" || password_text == "")) {
+                login_user(email_text, password_text)
+            }
+        })
 
-            if (email_text.equals("")) {
-                Toast.makeText(this, "Enter your email", Toast.LENGTH_SHORT).show();
-            }
-            if (password_text.equals("")) {
-                Toast.makeText(this, "Enter your password", Toast.LENGTH_SHORT).show();
-            }
-            if (!(email_text.equals("") || password_text.equals(""))) {
-                login_user(email_text, password_text);
-            }
-
-        });
-
-        signupLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), SignupActivity.class));
-            }
-        });
+        signupLink?.setOnClickListener(View.OnClickListener {
+            startActivity(
+                Intent(
+                    applicationContext, SignupActivity::class.java
+                )
+            )
+        })
     }
 
-    private void login_user(String email_text, String password_text) {
-        auth.signInWithEmailAndPassword(email_text, password_text).addOnCompleteListener(task -> {
-
-            if (task.isSuccessful()) {
-                startActivity(new Intent(getApplicationContext(), ChatHome.class));
-                Toast.makeText(LoginActivity.this, "Successfully logged in", Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(LoginActivity.this, "Login failed!", Toast.LENGTH_SHORT).show();
+    private fun login_user(email_text: String, password_text: String) {
+        auth!!.signInWithEmailAndPassword(email_text, password_text)
+            .addOnCompleteListener { task: Task<AuthResult?> ->
+                if (task.isSuccessful) {
+                    startActivity(Intent(applicationContext, ChatHome::class.java))
+                    Toast.makeText(this@LoginActivity, "Successfully logged in", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+                    Toast.makeText(this@LoginActivity, "Login failed!", Toast.LENGTH_SHORT).show()
+                }
             }
-        });
-
     }
 
-    private void signIn() {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
     }
 
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        auth.signInWithCredential(credential)
-                .addOnCompleteListener(this, task -> {
-                    if (task.isSuccessful()) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d("TAG", "signInWithCredential:success");
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth!!.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task: Task<AuthResult?> ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithCredential:success")
+                    startActivity(Intent(applicationContext, ChatHome::class.java))
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithCredential:failure", task.exception)
+                    Toast.makeText(applicationContext, "Invalid Login", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 
-                        startActivity(new Intent(getApplicationContext(), ChatHome.class));
-
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w("TAG", "signInWithCredential:failure", task.getException());
-                        Toast.makeText(getApplicationContext(), "Invalid Login", Toast.LENGTH_SHORT).show();
-
-                    }
-                });
+    companion object {
+        private const val RC_SIGN_IN = 1
+        var auth: FirebaseAuth? = null
+        var userid: String? = null
     }
 }
